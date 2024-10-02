979a04a95d796285c063eb683a98b84cd2db5e08
UK1\DucC <ducc@joblogic.com>
2024-10-02 18:19:04 +0700

    Update Login page

--- web/pages/login.vue
+++ web/pages/login.vue
@@ -1,3 +1,40 @@
 <template>
-    <div>Login Page</div>
+    <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
+        <UFormGroup label="Email" name="email">
+            <UInput v-model="state.email" />
+        </UFormGroup>
+
+        <UFormGroup label="Password" name="password">
+            <UInput v-model="state.password" type="password" />
+        </UFormGroup>
+
+        <UButton type="submit">Login</UButton>
+    </UForm>
 </template>
+
+<script setup lang="ts">
+import { object, string, ObjectSchema, type InferType } from "yup";
+import type { FormSubmitEvent } from "#ui/types";
+
+interface LoginRequest {
+    email: string;
+    password: string;
+}
+
+const schema: ObjectSchema<LoginRequest> = object({
+    email: string().required().email(),
+    password: string().required(),
+});
+
+type Schema = InferType<typeof schema>;
+
+const state = reactive({
+    email: "",
+    password: "",
+});
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+    // Do something with event.data
+    console.log(event.data);
+}
+</script>
