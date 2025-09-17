cf88c52ccbe507bd32dd3ed6cfb418704580e531
ducvt97 <ducvt97@gmail.com>
2025-09-18 00:33:58 +0700

    init gql apolo for web

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@pinia/nuxt", "pinia-plugin-persistedstate/nuxt"],
+  modules: [
+    "@nuxt/ui",
+    "@pinia/nuxt",
+    "pinia-plugin-persistedstate/nuxt",
+    "@nuxtjs/apollo",
+  ],
   css: ["~/assets/styles/main.scss"],
   dev: true,
   runtimeConfig: {
